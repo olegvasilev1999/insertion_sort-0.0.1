05084c52b6cffaae4d1dc083af613f57c181342e
olegvasilev1999 <32066946+olegvasilev1999@users.noreply.github.com>
2017-10-30 16:32:24 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,56 @@
+# insertion_sort-0.0.1
+
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+bool read_numbers(int & n, int * array)
+{
+	string string;
+	getline(cin, string);
+	istringstream stream(string);
+	bool success = true;
+	for (int i = 0; i < n; ++i) {
+		if (!(stream >> array[i])) {
+			success = false;
+			break;
+		}
+	}
+	return success;
+}
+
+int main() {
+	int n = 5;
+	int * array = new int;
+	int min;
+
+	string str;
+	getline(cin, str);
+	istringstream stream(str);
+	if (stream >> n) {
+		if (read_numbers(n, array)) {
+			int a, j; 
+			for (int i = 1; i < n; i++){
+				a = array[i];
+				j = i - 1; 
+				while (array[j] > a && j >= 0){
+					array[j + 1] = array[j];
+					array[j] = a;
+					j--;
+				}
+			}
+
+			for (int i = 0; i < n; i++) {
+				cout << array[i] << " ";
+			}
+		}
+		else {
+			cout << "An error has occured while reading input data.";
+		}
+	}
+
+	cin.get();
+	cin.get();
+	return 0;
+}
